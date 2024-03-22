68572a70100a19f9df655a10c648d2096c8f0469
h970920 <h970920@naver.com>
2024-03-22 20:18:00 +0900

    이미지 렌더링 테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import "./util.js";
+
+import { getEmotionById } from "./util.js";
 
 function App() {
   return (
     <div className="App">
       <h1>폰트테스트</h1>
+      <div>
+        <img alt="감정1" src={getEmotionById(1)} />
+        <img alt="감정1" src={getEmotionById(2)} />
+        <img alt="감정1" src={getEmotionById(3)} />
+        <img alt="감정1" src={getEmotionById(4)} />
+        <img alt="감정1" src={getEmotionById(5)} />
+      </div>
     </div>
   );
 }
