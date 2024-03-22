228a17c50a8c0ed1825ebff78e4bf2f2cdfdbd72
h970920 <h970920@naver.com>
2024-03-22 20:08:04 +0900

    폰트 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>폰트테스트</h1>
+    </div>
+  );
 }
 
 export default App;
