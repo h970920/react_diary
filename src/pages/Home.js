8febca9a352abca3ef5f47cc083efe45680165b1
h970920 <h970920@naver.com>
2024-03-24 19:53:14 +0900

    home.js도 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("sort"));
+
   return (
     <div>
       <h1>Home</h1>
