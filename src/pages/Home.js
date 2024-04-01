ddae7699fbf9615b7c7580072320784252100ddb
h970920 <h970920@naver.com>
2024-04-01 10:05:10 +0900

    홈js에 에디터 렌더링

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,9 @@
 import React from 'react'; // React를 import 해주세요.
-import Button from '../component/Button';
-import Header from '../component/Header';
-
+import Editor from '../component/Editor';
 const Home = () => {
     return (
         <div>
-            <Header
-                title={'Home'}
-                leftChild={
-                    <Button
-                        type="positive"
-                        text={'긍정버튼'}
-                        onClick={() => {
-                            alert('긍정버튼 클릭!');
-                        }}
-                    />
-                }
-                rightChild={
-                    <Button
-                        type="negative"
-                        text={'부정버튼'}
-                        onClick={() => {
-                            alert('부정버튼 클릭!');
-                        }}
-                    />
-                }
-            />
+            <Editor />
         </div>
     );
 };
