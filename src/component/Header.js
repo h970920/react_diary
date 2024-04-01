86b1449678db3c6645a9e0a48fc50f63dc5486c8
h970920 <h970920@naver.com>
2024-04-01 09:44:55 +0900

    헤더 렌더링/구현 완료

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,11 @@
-import 'Header.css';
+import './Header.css';
 const Header = ({ title, leftChild, rightChild }) => {
     return (
-        <header>
-            <div className="header-left">{leftChild}</div>
-            <div className="header-title">{title}</div>
-            <div className="header-right">{rightChild}</div>
-        </header>
+        <div className="Header">
+            <div className="header_left">{leftChild}</div>
+            <div className="header_title">{title}</div>
+            <div className="header_right">{rightChild}</div>
+        </div>
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,33 @@
-import Button from "../component/Button";
+import React from 'react'; // React를 import 해주세요.
+import Button from '../component/Button';
+import Header from '../component/Header';
 
 const Home = () => {
-  return (
-    <div>
-      <Button
-        type={"default"}
-        text={"기본 버튼"}
-        onClick={() => {
-          alert("default button clicked");
-        }}
-      />
-
-      <Button
-        type={"positive"}
-        text={"긍정 버튼"}
-        onClick={() => {
-          alert("positive button clicked");
-        }}
-      />
-
-      <Button
-        type={"negative"}
-        text={"부정 버튼"}
-        onClick={() => {
-          alert("nagative button clicked");
-        }}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Header
+                title={'Home'}
+                leftChild={
+                    <Button
+                        type="positive"
+                        text={'긍정버튼'}
+                        onClick={() => {
+                            alert('긍정버튼 클릭!');
+                        }}
+                    />
+                }
+                rightChild={
+                    <Button
+                        type="negative"
+                        text={'부정버튼'}
+                        onClick={() => {
+                            alert('부정버튼 클릭!');
+                        }}
+                    />
+                }
+            />
+        </div>
+    );
 };
 
 export default Home;
